e69eb1eb7f6a41bad71012a45c5d5a6b5cb0b526
Zeeroh <zerodeluxetm@gmail.com>
2020-06-23 18:29:11 -0400

    Update AssemblyInfo.cs

--- UnityLoader/Properties/AssemblyInfo.cs
+++ UnityLoader/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TweakLoader-CH")]
+[assembly: AssemblyTitle("UnityLoader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TweakLoader-CH")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("UnityLoader")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
